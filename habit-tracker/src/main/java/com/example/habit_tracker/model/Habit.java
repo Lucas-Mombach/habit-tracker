2a13ddb5e7e4c77c7ccab3987eb8dbbc58e7a0d0
Lucas Mombach <lucasmmombach@gmail.com>
2024-06-29 16:45:25 -0300

    add getters and setters

--- habit-tracker/src/main/java/com/example/habit_tracker/model/Habit.java
+++ habit-tracker/src/main/java/com/example/habit_tracker/model/Habit.java
@@ -3,6 +3,14 @@ package com.example.habit_tracker.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Document(collection = "habits")
 public class Habit {
   @Id
