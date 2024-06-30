7222a60ee3c0c39366e54b8c3b044f13b58de1c8
Lucas Mombach <lucasmmombach@gmail.com>
2024-06-30 10:36:14 -0300

    create habitController and add getAllHabits

---
+++ habit-tracker/src/main/java/com/example/habit_tracker/controller/HabitController.java
@@ -0,0 +1,39 @@
+package com.example.habit_tracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.habit_tracker.model.Habit;
+import com.example.habit_tracker.service.HabitService;
+
+@RestController
+@RequestMapping("/api/v1/habits")
+public class HabitController {
+  @Autowired
+  private HabitService habitService;
+
+  @GetMapping
+  public List<Habit> getAllHabits() {
+        return habitService.getAllHabits();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Habit> getHabitById(@PathVariable String id) {
+    return habitService.getHabitById(id)
+    .map(ResponseEntity::ok)
+    .orElse(ResponseEntity.notFound().build());
+  }
+  @PostMapping
+  public Habit createHabit(@RequestBody Habit habit){
+    return habitService.createHabit(habit);
+  }
+
+}
