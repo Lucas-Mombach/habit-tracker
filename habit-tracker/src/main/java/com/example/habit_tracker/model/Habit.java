3a7975c126e332bbb37655647a112d680f4007d8
Lucas Mombach <lucasmmombach@gmail.com>
2024-07-05 14:07:33 -0300

    add ToString method

--- habit-tracker/src/main/java/com/example/habit_tracker/model/Habit.java
+++ habit-tracker/src/main/java/com/example/habit_tracker/model/Habit.java
@@ -7,10 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @Document(collection = "habits")
 public class Habit {
   @Id
